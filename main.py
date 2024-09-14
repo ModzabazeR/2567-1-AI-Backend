8a43517960e888cff689876e99fb1928310ab300
kemkai <kemkaikub@gmail.com>
2024-09-15 01:17:46 +0700

    create endpoint for upload and show caption

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
-from flask import Flask, jsonify
-import service.captiongen as capgen
+from flask import Flask, flash, jsonify, render_template, request, redirect, url_for
+import service.captiongen as capgen, os
 
 app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = 'static/'
 
-def hello():
-    return 'Hello, World!'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
-@app.route('/', methods=['GET'])
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
 def home():
-    result = capgen.hello()
-    return jsonify({'message': result})
+    return render_template('index.html')
 
-@app.route('/books', methods=['GET'])
-def get_books():
-    books = [{'id': 1, 'title': 'Python Essentials', 'author': 'Jane Doe'}]
-    return jsonify({'books': books})
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        print("Not found")
+        flash('No file part')
+        return redirect(request.url)
+    
+    file = request.files['file']
+    if file.filename == '':
+        flash('No selected file')
+        return redirect(request.url)
+    print("Get DIr: ", dir(file))
+    print(file.content_type)
+    print(file.filename)
+    if file and allowed_file(file.filename):
+        filename = file.filename
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # return render_template('index.html', filename=filename)
+        
+        caption = capgen.generateCaption(filename)
+        
+        return render_template('index.html', filename=filename, caption=caption)
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
--- service/captiongen.py
+++ service/captiongen.py
@@ -1,2 +1,6 @@
 def hello():
 	return "Hello, World!"
+
+def generateCaption(filename) :
+    print(filename)
+    return "Caption for " + filename
